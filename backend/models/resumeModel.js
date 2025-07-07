8e641daa34e04fd1eb51f0b407f7bc48f6c464e0
kumarmanish562 <qmanishkumar562@gmail.com>
2025-07-07 10:15:46 +0530

    Refactors resume model structure and adds new fields
    Updates the resume model to include a thumbnail link and a nested templates object for theme and color palette.
    
    Replaces project links with more descriptive fields and modifies the certifications structure to include year instead of specific date fields.
    
    Removes unused references section and standardizes timestamp fields for better consistency.

--- backend/models/resumeModel.js
+++ backend/models/resumeModel.js
@@ -1,19 +1,27 @@
 import mongoose from "mongoose";
 
 const resumeSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId,
+     ref: "User", 
+     required: true },
   title: {
     type: String,
     required: true,
   },
-  theme: String,
-  colorPalette: [String],
+  thumbLink: {
+    type: String,
+  },
+  templates: {
+    theme: String,
+    colorPalette: [String],
+  },
   profileInfo: {
     profilePreviewUrl: String,
     FullName: String,
     designation: String,
     summary: String,
   },
+
   contactInfo: {
     email: String,
     phone: String,
@@ -32,6 +40,7 @@ const resumeSchema = new mongoose.Schema({
       description: String,
     }
   ],
+
   education: [
     {
       degree: String,
@@ -40,50 +49,43 @@ const resumeSchema = new mongoose.Schema({
       endDate: Date,
     }
   ],
+
   skills: [
     {
       name: String,
       progress: Number, 
     }
   ],
+
   project: [
     {
       title: String,
       description: String,
-      githubLink: String,
-      liveLink: String,
-      startDate: Date,
-      endDate: Date,
-      link: String,
-    }
+      github: String,
+      liveDemo: String,
+    },
   ],
+
   certifications: [
     {
       title: String,
       issuer: String,
-      issueDate: Date,
-      expirationDate: Date,
-      credentialId: String,
-      credentialUrl: String,
+     year: String,
     }
   ],
+
+
   languages: [
     {
       name: String,
-      proficiency: String,
+      progress: Number, 
     }
   ],
+
   interests: [String],
-  references: [
-    {
-      name: String,
-      relationship: String,
-      contactInfo: {
-        email: String,
-        phone: String,
-      },
-    }
-  ],
-}, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
+    
+}, { 
+  timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } 
+});
 
 export default mongoose.model("Resume", resumeSchema);
