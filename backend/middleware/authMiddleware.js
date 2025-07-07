f0b2a2eba209d407303df3dae5c91136c9ad10b6
kumarmanish562 <qmanishkumar562@gmail.com>
2025-07-07 10:26:51 +0530

    Improves token verification in auth middleware
    Updates the `protect` middleware to use async/await for user retrieval, ensuring proper handling of asynchronous operations.
    
    This change enhances the token verification process by correctly checking for the presence of a Bearer token and retrieving the user from the database.
    
    Improves error handling and authorization response.

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,21 +2,21 @@ import User from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
 
 
-export const protect = (req, res, next) => {
-  try{
+export const protect = async (req, res, next) => {
+  try {
     let token = req.headers.authorization;
-    if (!token && token.startsWith('Bearer')) {
+    
+    if (token && token.startsWith('Bearer')) {
       token = token.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = User.findById(decoded.id).select('-password');
+      req.user = await User.findById(decoded.id).select('-password');
       next();
-    }
-    else {
+    } else {
       return res.status(401).json({ message: 'Not authorized, no token' });
     }
   } catch (error) {
     return res.status(401).json({ message: 'Not authorized, token failed' });
   }
-}
+};
 
     
