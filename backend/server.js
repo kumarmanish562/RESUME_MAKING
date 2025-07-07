01931b8ea19ebc26fc4589605acf7f9ed6e6f1ca
kumarmanish562 <qmanishkumar562@gmail.com>
2025-07-07 10:06:58 +0530

    feat: add resume routes and static file serving for uploads

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,14 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
+import resumeRoutes from './routes/resumeRoutes.js';
 
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -20,6 +26,13 @@ app.use(express.json());
 
 
 app.use('/api/auth', userRoutes);
+app.use('/api/resume', resumeRoutes);
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'), {
+  setHeaders: (res, path) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+  }
+}));
 
 // ROUTES
 app.get('/', (req, res) => {
