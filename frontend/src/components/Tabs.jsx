abbb6b815fa332ae7ab88b82d1b4b0b053d1858f
kumarmanish562 <qmanishkumar562@gmail.com>
2025-07-13 10:22:52 +0530

    Adds forms and templates for resume builder
    Implements various components for a resume builder, including forms for additional information, certifications, contact details, education, profile, projects, skills, and work experience.
    
    Introduces a template selection interface to enhance user experience in customizing resumes.
    
    These changes facilitate the creation and management of user profiles, improving overall functionality and organization within the application.

---
+++ frontend/src/components/Tabs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+  return (
+    <div className='w-full my-2'>
+      <div>
+        <div className='flex flex-wrap bg-violet-50 p-1 rounded-2xl border border-violet-100'>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Tabs
---
+++ frontend/src/components/ThemeSelector.jsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from 'react'
+import { resumeTemplates} from '../utils/data'
+import Tabs from './Tabs'
+import { Check } from 'lucide-react'
+
+
+const TAB_DATA = [{label: 'Templates' }]
+
+const ThemeSelector = ([selectedTheme, setSelectedTheme, resumeData, onClose]) => {
+  const resumeRef= useRef(null)
+  const[baseWidth, setBaseWidth] = useState(800);
+
+  // SELECTED THEME TEMPLATE USING ID 
+  const initialIndex = resumeTemplates.findIndex(t => t.id === selectedTheme);
+  const [selectedTemplate, setSelectedTemplate] = useState({
+    theme: selectedTheme || resumeTemplates[0]?.id || '',
+    index: initialIndex >= 0 ? initialIndex : 0
+  })
+
+  const [tabValue, setTabValue] = useState('Templates');
+
+  const handleThemeSeclection = () => {
+    setSelectedTemplate(selectedTemplate.theme)
+    onClose();
+  }
+
+  const updateBaseWidth = () => {
+    if (resumeRef.current) {
+      setBaseWidth(resumeRef.current.offsetWidth);
+    }
+  }
+  return (
+    <div className='max-w-7xl mx-auto p-4'>
+      {/* header */}
+      <div className='flex flex-col sm:flex-row items-start sm:items-center justify-between mb-s8 p-4 sm:p-6 bg-gradient-to-r from-white  to-violet-50 rounded-2xl border border-violet-100 '>
+
+        <Tabs  tabs={TAB_DATA}  activeTab={tabValue} setTabValue={setTabValue} />
+
+        <button className='w-full sm:w-auto flex items-center justify-center gap-3 px-6 py-3 bg-gradient-to-r from-violet-600 to-fuchsia-600 text-white font-black rounded-2xl hover:scale-105 trasnition-all shadow-lg hover:shadow-xl' onClick={handleThemeSeclection}>
+          <Check size={18} />
+          Apply Changes
+        </button>
+      </div>
+      <div className='grid grid-cols-1  lg:grid-cols-5 gap-6 lg:gap-8'>
+        <div className='lg:col-span-2 bg-white rounded-xl border border-gray-100 p-4 sm:p-6'>
+
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default ThemeSelector
